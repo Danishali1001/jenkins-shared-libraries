aba271ae117da44a43fe7b170b898adb661b0f61
Danish Ali <ali9548061@gmail.com>
2026-06-15 11:42:50 +0530

    Refactor docker_build function for Weather-Application

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,6 @@
-// Define function
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(imageName, tag, dockerHubUser) {
+    sh """
+        cd Weather-Application
+        docker build -t ${dockerHubUser}/${imageName}:${tag} .
+    """
 }
